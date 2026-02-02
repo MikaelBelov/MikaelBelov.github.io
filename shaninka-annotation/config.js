316949ad878028e0b9fa3d6a8a1240ed602fb944
MikaelBelov <misha6041@gmail.com>
2026-02-01 16:51:08 -0800

    Update Apps Script URL in config.js

--- shaninka-annotation/config.js
+++ shaninka-annotation/config.js
@@ -1,5 +1,5 @@
 // 🔒 Конфигурация БЕЗ публичных API ключей
 const CONFIG = {
     // Только URL Apps Script - всё остальное внутри Apps Script!
-    appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyRCAFPFLaGtyIzFeT_4Ey9MOZToQuzmFz2eeJnjBc3pKKVROZ9kS7yJm6g5zmryzpI/exec'
+    appsScriptUrl: 'https://script.google.com/macros/s/AKfycbwZoz23sxiq6cwZmae5MWykmX_6DGxJS-aT9ldmzbShrC9m9S-NzrzgBbIN8veWPqig/exec'
 };
