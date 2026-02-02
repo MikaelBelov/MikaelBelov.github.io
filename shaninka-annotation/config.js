939a55d564e95680032e446c109a0058bde14c4b
MikaelBelov <misha6041@gmail.com>
2026-02-01 18:02:42 -0800

    Change Apps Script URL in config.js
    Updated the Apps Script URL in the configuration.

--- shaninka-annotation/config.js
+++ shaninka-annotation/config.js
@@ -1,7 +1,7 @@
 // 🔒 Конфигурация БЕЗ публичных API ключей
     // Только URL Apps Script - всё остальное внутри Apps Script!
 const CONFIG = {
-    appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyZNz4BUiWykZNfGuf5-jZGiRUmzk10HC-VuQ2KR7D16O2VThEbvDeDqMIpOQR9c1AKPw/exec'
+    appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxS83QUXa1nPM42WkksgzqwranAXYIhcf6VQ8Zq6IY95MfeT9BiKzDCvYqKJIgc6arBtA/exec'
     
     // Google OAuth Client ID
     googleClientId: '342111189232-pb3cht7qdnko5idjfk17ae8e74mb3hvk.apps.googleusercontent.com'
