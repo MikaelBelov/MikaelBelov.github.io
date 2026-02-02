02cf275d4c400cb9277a492a6d3fbeec41988d1d
MikaelBelov <misha6041@gmail.com>
2026-02-01 17:53:22 -0800

    Add Google OAuth Client ID and Apps Script URL

--- shaninka-annotation/config.js
+++ shaninka-annotation/config.js
@@ -1,5 +1,8 @@
 // 🔒 Конфигурация БЕЗ публичных API ключей
-const CONFIG = {
     // Только URL Apps Script - всё остальное внутри Apps Script!
+const CONFIG = {
     appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyZNz4BUiWykZNfGuf5-jZGiRUmzk10HC-VuQ2KR7D16O2VThEbvDeDqMIpOQR9c1AKPw/exec'
+    
+    // Google OAuth Client ID
+    googleClientId: '342111189232-pb3cht7qdnko5idjfk17ae8e74mb3hvk.apps.googleusercontent.com'
 };
