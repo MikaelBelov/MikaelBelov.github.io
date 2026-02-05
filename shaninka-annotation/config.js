4441122946c6cea2ddaebbd4e84eec6db4d0ce1a
Michael <michael@vpn-172-27-47-23.host.ucla.edu>
2026-02-04 18:49:53 -0800

    Update Apps Script deployment URL

--- shaninka-annotation/config.js
+++ shaninka-annotation/config.js
@@ -1,7 +1,7 @@
 // 🔒 Конфигурация БЕЗ публичных API ключей
     // Только URL Apps Script - всё остальное внутри Apps Script!
 const CONFIG = {
-    appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxuKG3GbQS5op-OnAZy0f0r8KBYje6r1fK9mUk1TkvEa6hgpVXM_1erlBnbebtnZUrzjA/exec',
+    appsScriptUrl: 'https://script.google.com/macros/s/AKfycbwDSI5OfKdlAcnF7Jpg_2jleTC_YRTJZyuVBkCts1Hk95D8OtHo8r1OB-Fcy5Le2LSH1A/exec',
     
     // Google OAuth Client ID
    // googleClientId: '342111189232-pb3cht7qdnko5idjfk17ae8e74mb3hvk.apps.googleusercontent.com'
